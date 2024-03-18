45aaa8304e388de69bc8900101f3846ef846b3c5
Hoàn Bùi <hoan.bui24@student.passerellesnumeriques.org>
2024-03-18 20:42:47 +0700

    Edit the function Recommend Store

--- src/routes/shopRoute.js
+++ src/routes/shopRoute.js
@@ -2,10 +2,11 @@ const express = require('express')
 const multer = require('multer')
 const upload = multer()
 
-const { getShopById } = require('../controllers/shopController')
+const { getShopById,getDataProductByShopId } = require('../controllers/shopController')
 
 const router = express.Router()
 
 router.get('/getShopById/:shopId',upload.none(),getShopById)
+router.get('/getDataProductByShopId/:shopId',upload.none(),getDataProductByShopId)
 
 module.exports = router
